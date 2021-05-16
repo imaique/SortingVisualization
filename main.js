22e9d5d65f9a0fd453c75d96b77d7b138810a239
Michael <osujim2012@gmail.com>
2021-05-16 00:15:15 -0400

    Adding sorting algorithm selection

--- main.js
+++ main.js
@@ -2,16 +2,48 @@ var board;
 var mainArray;
 var nbOfElements = 50;
 var range = 30;
-var defaultLineColor = "#4D5061";
-var noMatchLineColor = "#EF626C";
-var matchLineColor = "#5C80BC";
-var completeLineColor = "#688B58"
-const baseSpeed = 50;
+const defaultLineColor = "#4D5061";
+const noMatchLineColor = "#EF626C";
+const matchLineColor = "#5C80BC";
+const completeLineColor = "#688B58";
+const touchedColor = "#8E7DBE";
+
+const baseSpeed = 25;
 var selectedSpeed = baseSpeed;
 var isComplete;
 var isStarted = false;
 const speedInput = document.getElementById("speed");
 speedInput.addEventListener('input', updateSpeed);
+const sortingTypes = [[algoName = "Selection", functionName = selectionSort], [algoName = "Insertion", functionName = insertionSort]
+    , [algoName = "Bubble", functionName = bubbleSort]];
+const sortingDOM = document.getElementById("algorithmPick");
+var selectedSorting = selectionSort;
+
+function createSortingInputs(sortingTypes, sortingDOM) {
+    sortingTypes.forEach(e => {
+        const [name, func] = e;
+        let inputDOM = document.createElement("input");
+        inputDOM.type = "radio";
+        inputDOM.name = "algorithm";
+        inputDOM.id = name;
+        inputDOM.value = name;
+        if (func === selectedSorting) inputDOM.checked = "checked";
+        inputDOM.addEventListener("change", function () {
+            if (selectedSorting !== func) {
+                selectedSorting = func;
+            }
+        });
+
+
+        let labelDOM = document.createElement("label");
+        labelDOM.for = name;
+        labelDOM.textContent = name + " Sort";
+
+        sortingDOM.appendChild(inputDOM);
+        sortingDOM.appendChild(labelDOM);
+    }
+    );
+}
 
 /*
 possible solutions to visualization Promise.all
@@ -26,15 +58,21 @@ class LineObject {
         this.height = height;
         this.element.style.height = height * 10 + "px";
     }
+    setColor(color) {
+        this.element.style.backgroundColor = color;
+    }
 }
 function updateSpeed() {
     selectedSpeed = baseSpeed / (speedInput.value / 2);
-    console.log(speedInput.value)
-    console.log(selectedSpeed)
-
 }
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function colorAllGreen() {
+    mainArray.forEach(element => {
+        element.style.backgroundColor = completeLineColor;
+    });
 }
 async function startPress() {
     if (isStarted) return;
@@ -45,20 +83,54 @@ async function startPress() {
     -include generate new array
     -one choice checklist to chose algorithm with if statements in here
     */
-    await selectionSort(selectedSpeed);
+    // it's a switch statement for now, but you could find a way to store a function in each radios
+    await selectedSorting(selectedSpeed);
+    for (const line of mainArray) {
+        line.element.style.backgroundColor = completeLineColor;
+        await sleep(15);
+    }
+
     isStarted = false;
     isComplete = true;
 }
 
-async function selectionSort(speed) {
+// slightly optimized bubble sort
+async function bubbleSort(speed) {
+    let n = mainArray.length - 1;
+    let isSwap;
+    do {
+        isSwap = false;
+        for (let i = 0; i < n; i++) {
+            mainArray[i].element.style.backgroundColor = matchLineColor;
 
+            if (mainArray[i].height > mainArray[i + 1].height) {
+                mainArray[i + 1].setColor(matchLineColor);
+                await sleep(speed);
+                switchLines(i, i + 1);
+                mainArray[i + 1].element.style.backgroundColor = defaultLineColor;
+                await sleep(speed);
+                isSwap = true;
+            }
+            else {
+                mainArray[i + 1].element.style.backgroundColor = noMatchLineColor;
+                await sleep(speed);
+            }
+            mainArray[i].element.style.backgroundColor = defaultLineColor;
+        }
+        mainArray[n].element.style.backgroundColor = completeLineColor;
+        n--;
+
+    } while (isSwap)
+
+}
+
+async function selectionSort(speed) {
     for (let i = 0; i < mainArray.length; i++) {
-        console.log(i);
-        mainArray[i].element.style.backgroundColor = matchLineColor;
+        let mainColor =
+            mainArray[i].element.style.backgroundColor = matchLineColor;
         let min = mainArray[i].height;
         let minIndex = i;
         for (let j = i + 1; j < mainArray.length; j++) {
-            console.log(i + " vs " + j)
             if (min > mainArray[j].height) {
                 min = mainArray[j].height;
                 if (minIndex != i) mainArray[minIndex].element.style.backgroundColor = defaultLineColor;
@@ -73,20 +145,60 @@ async function selectionSort(speed) {
 
         }
         if (minIndex !== i) {
-            await sleep(speed);
-            await switchLines(i, minIndex);
-            await sleep(speed);
+            await sleepSwitchLines(i, minIndex, speed);
+            mainArray[minIndex].element.style.backgroundColor = defaultLineColor;
         }
         mainArray[i].element.style.backgroundColor = completeLineColor;
     }
 }
+// finish
+async function insertionSort(speed) {
+    // for [x][y][z]
+    for (let i = 1; i < mainArray.length; i++) {
+        // let currentHeight = [z]
+        mainArray[i].element.style.backgroundColor = matchLineColor;
+        let currentHeight = mainArray[i].height;
+        // [z] < [y] ?
+        if (currentHeight < mainArray[i - 1].height) {
+            // [z] [y]
+            mainArray[i].element.style.backgroundColor = matchLineColor;
+            await sleepSwitchLines(i - 1, i, speed);
+            mainArray[i].element.style.backgroundColor = touchedColor;
+            // [x]
+            let j = i - 2;
+            // [z] < [x]
+            while (j >= 0) {
+                if (currentHeight < mainArray[j].height) {
+                    mainArray[j].element.style.backgroundColor = matchLineColor;
+                    await sleepSwitchLines(j, j + 1, speed);
+                    mainArray[j + 1].element.style.backgroundColor = touchedColor;
+                    j--;
+                }
+                else {
+                    mainArray[j].element.style.backgroundColor = noMatchLineColor;
+                    await sleep(speed);
+                    mainArray[j].element.style.backgroundColor = touchedColor;
+                    mainArray[j + 1].element.style.backgroundColor = touchedColor;
+                    break;
+                }
+            }
 
+        } else (mainArray[i - 1].element.style.backgroundColor = noMatchLineColor);
+    }
+
+}
+
+async function sleepSwitchLines(i, j, speed) {
+    await sleep(speed);
+    switchLines(i, j);
+    await sleep(speed);
+}
 async function switchLines(i, j) {
     let initialHeight = mainArray[i].height;
 
     mainArray[i].setNewHeight(mainArray[j].height);
     mainArray[j].setNewHeight(initialHeight);
-    mainArray[j].element.style.backgroundColor = defaultLineColor;
+
 
 }
 
@@ -111,7 +223,7 @@ function createLines() {
 
 }
 
-
+createSortingInputs(sortingTypes, sortingDOM);
 board = document.getElementById("hList");
 createLines();
 
